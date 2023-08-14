334812ad17621932203f8dd16797344933f83e7a
Hemin200556501 <137121004+Hemin200556501@users.noreply.github.com>
2023-08-14 10:49:24 -0400

    Add files via upload

---
+++ Final Exam/js/script.js
@@ -0,0 +1,35 @@
+// Function to include HTML content using AJAX
+function includeHTML() {
+    // Create a new XMLHttpRequest object
+    const xhttp = new XMLHttpRequest();
+
+    // Define a callback function to handle the response
+    xhttp.onreadystatechange = function() {
+        // Check if the request is complete and the response is successful
+        if (this.readyState == 4 && this.status == 200) {
+            // Update the content of the element with ID 'header' with the received HTML
+            document.getElementById('header').innerHTML = this.responseText;
+        }
+    };
+
+    // Open a GET request to retrieve 'header.html'
+    xhttp.open('GET', 'header.html', true);
+
+    // Send the request
+    xhttp.send();
+}
+
+// Call the includeHTML function to include the HTML content
+includeHTML();
+
+function loadFooter() {
+    const footerContainer = document.getElementById('footer-container');
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'footer.html', true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        footerContainer.innerHTML = xhr.responseText;
+      }
+    };
+    xhr.send();
+  }
